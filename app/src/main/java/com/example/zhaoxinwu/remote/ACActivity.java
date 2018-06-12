f1d8e89b730049e01aae4896fca5e71b28b13347
YagiGo <jeremywu1995@gmail.com>
2018-06-12 13:45:16 +0900

    UI design for TV,AC,lamp remote

--- app/src/main/java/com/example/zhaoxinwu/remote/ACActivity.java
+++ app/src/main/java/com/example/zhaoxinwu/remote/ACActivity.java
@@ -2,12 +2,19 @@ package com.example.zhaoxinwu.remote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ACActivity extends AppCompatActivity {
+    private TextView mACTemp, mRoomTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ac);
+        mACTemp = (TextView)findViewById(R.id.value_ac_temp);
+
+        Button tempUp = (Button)findViewById(R.id.button_)
+
     }
 }
--- app/src/main/java/com/example/zhaoxinwu/remote/MainActivity.java
+++ app/src/main/java/com/example/zhaoxinwu/remote/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         acRemoteSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), LampActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ACActivity.class);
                 startActivity(intent);
             }
         });
