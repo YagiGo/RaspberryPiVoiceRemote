caf372d8a337544d82fb81b1f1e8d99627697866
YagiGo <jeremywu1995@gmail.com>
2018-06-12 17:25:05 +0900

    UI design for TV,AC,lamp remote, function implement

--- app/src/main/java/com/example/zhaoxinwu/remote/LampActivity.java
+++ app/src/main/java/com/example/zhaoxinwu/remote/LampActivity.java
@@ -4,10 +4,18 @@ import android.content.Context;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class LampActivity extends AppCompatActivity {
 
@@ -17,40 +25,47 @@ public class LampActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lamp);
         context = getApplicationContext();
+        final RequestQueue queue = Volley.newRequestQueue(this);
         ImageButton buttonPower = findViewById(R.id.button_lamp_power);
         ImageButton buttonBrightlessUP = findViewById(R.id.button_lamp_increase_brightless);
         ImageButton buttonBrightlessDOWN = findViewById(R.id.button_lamp_decrease_brightless);
         Button buttonWarmLight = findViewById(R.id.button_lamp_warm);
         Button buttonColdLight = findViewById(R.id.button_lamp_cold);
+        final String urlLamp = "http://192.168.0.3:5000/remote/iris_oyama.light/";
 
         buttonPower.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "Lamp will be powered on/off", Toast.LENGTH_SHORT).show();
+                sendRequest(queue, urlLamp, "KEY_POWER");
             }
         });
         buttonBrightlessUP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "Lamp will be brighter", Toast.LENGTH_SHORT).show();
+                sendRequest(queue, urlLamp, "KEY_BRIGHTNESSUP");
             }
         });
         buttonBrightlessDOWN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "Lamp will be dimmer", Toast.LENGTH_SHORT).show();
+                sendRequest(queue, urlLamp, "KEY_BRIGHTNESSDOWN");
             }
         });
         buttonWarmLight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "Warm Light!", Toast.LENGTH_SHORT).show();
+                sendRequest(queue, urlLamp, "KEY_RED");
             }
         });
         buttonColdLight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "Cold Light", Toast.LENGTH_SHORT).show();
+                sendRequest(queue, urlLamp, "KEY_BLUE");
             }
         });
     }
@@ -61,5 +76,23 @@ public class LampActivity extends AppCompatActivity {
     @Override
     protected void onPause(){
         super.onPause();
+    }
+    public void sendRequest(RequestQueue queue, String url, String param) {
+        StringRequest putRequest = new StringRequest(Request.Method.PUT,
+                url+param,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("RESPONSE", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("ERROR",error.toString());
+                    }
+                });
+
+        queue.add(putRequest);
     }
 }
--- app/src/main/java/com/example/zhaoxinwu/remote/TVActivity.java
+++ app/src/main/java/com/example/zhaoxinwu/remote/TVActivity.java
@@ -2,16 +2,32 @@ package com.example.zhaoxinwu.remote;
 
 import android.content.Context;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+
 public class TVActivity extends AppCompatActivity {
     Context context;
+    private String urlTV = "http://192.168.0.3:5000/remote/pioneer.tv/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final RequestQueue queue = Volley.newRequestQueue(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv);
         context = getApplicationContext();
@@ -27,6 +43,25 @@ public class TVActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "TV will be powered on/off", Toast.LENGTH_SHORT).show();
+                /*
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, urlTV,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Log.d("Response", response.substring(0, 500));
+                            }
+                        },
+
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("Didn","Didn;t work");
+                            }
+                        }
+                );
+                queue.add(stringRequest);
+                */
+                sendRequest(queue, urlTV, "KEY_POWER");
 
             }
         });
@@ -35,6 +70,8 @@ public class TVActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "TV will be muted", Toast.LENGTH_SHORT).show();
+                sendRequest(queue, urlTV, "KEY_MUTE");
+
 
             }
         });
@@ -42,6 +79,7 @@ public class TVActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "TV menu will be opened", Toast.LENGTH_SHORT).show();
+                sendRequest(queue, urlTV, "KEY_MENU");
 
             }
         });
@@ -49,6 +87,7 @@ public class TVActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "TV volume will be turned up", Toast.LENGTH_SHORT).show();
+                sendRequest(queue, urlTV, "KEY_VOLUMEUP");
 
             }
         });
@@ -56,6 +95,7 @@ public class TVActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "TV volume will be turned down", Toast.LENGTH_SHORT).show();
+                sendRequest(queue, urlTV, "KEY_VOLUMEDOWN");
 
             }
         });
@@ -63,17 +103,18 @@ public class TVActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "Back to previous channel", Toast.LENGTH_SHORT).show();
-
+                sendRequest(queue, urlTV, "KEY_LEFT");
             }
         });
         buttonNextChannel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "Next channel", Toast.LENGTH_SHORT).show();
-
+                sendRequest(queue, urlTV, "KEY_RIGHT");
             }
         });
     }
+
     @Override
     protected void onResume(){
         super.onResume();
@@ -82,4 +123,24 @@ public class TVActivity extends AppCompatActivity {
     protected void onPause(){
         super.onPause();
     }
+
+    public void sendRequest(RequestQueue queue, String url, String param) {
+        StringRequest putRequest = new StringRequest(Request.Method.PUT,
+                url+param,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("RESPONSE", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("ERROR",error.toString());
+                    }
+        });
+
+        queue.add(putRequest);
+    }
+
 }
